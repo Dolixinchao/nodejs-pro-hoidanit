1d78aeea674e6b9ee366c34cac3c017ab2a1dcce
Siki Coder <tonnasa2005@gmail.com>
2025-07-21 17:00:59 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`)
+})
